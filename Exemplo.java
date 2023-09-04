6598dba082b85b1ae78099e6d2203420483ee02b
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2023-09-04 22:06:07 +0000

    Revisão OO

---
+++ Exemplo.java
@@ -0,0 +1,13 @@
+public class Exemplo {
+    public static void main(String[] args){
+        Pessoa pes = new Pessoa();
+        pes.setNome("Marcelo Zanguettin");
+
+        PessoaFisica pf = new PessoaFisica();
+        pf.setNome("Fulano");
+        pf.setCpf("1326132");
+
+        System.out.println("Olá Mundo!!");
+        System.out.println(pes.getNome());
+    }
+}
